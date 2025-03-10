e31161a8941f68d2389f7a6c1e6eb9e55619d89f
SIC1740 <SiVV.B21VT378@stu.ptit.edu.vn>
2025-03-11 00:22:32 +0700

    BASIC001

---
+++ src/BASIC001.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+// Luu y khi nop nho doi ten class thành Main
+public class BASIC001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        while (a-- >0){
+            Scanner sc1 = new Scanner(System.in);
+            String s = sc1.nextLine();
+            System.out.println(s);
+        }
+        sc.close();
+
+    }
+
+}
