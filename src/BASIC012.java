8c726a8b688bba969893ec679b35d0992743a387
SIC1740 <SiVV.B21VT378@stu.ptit.edu.vn>
2025-03-13 14:46:27 +0700

    BASIC0012

--- src/BASIC012.java
+++ src/BASIC012.java
@@ -1,3 +1,38 @@
-public class BASIC012 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class BASIC012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            ArrayList<Integer> li = new ArrayList<>();
+            String[] s = sc.nextLine().split(" ");
+
+            for(String str : s) {
+                li.add(Integer.parseInt(str));
+            }
+            if(li.isEmpty()) {
+                System.out.println("0");
+                continue;
+            }
+            int maxLen = 1;
+            int currentLen = 1;
+            for(int i = 1; i < li.size(); i++) {
+                if(li.get(i) >= li.get(i-1)) {
+                    currentLen++;
+                }
+                else {
+                    if(currentLen > maxLen) {
+                        maxLen = currentLen;
+                    }
+                    currentLen = 1;
+                }
+            }
+            maxLen = Math.max(maxLen, currentLen);
+            System.out.println(maxLen);
+        }
+        sc.close();
+    }
 }
