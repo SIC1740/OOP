518478b713fd14f666a89adc04df31a3d4610e9a
SIC1740 <SiVV.B21VT378@stu.ptit.edu.vn>
2025-03-16 18:11:42 +0700

    STRING003

--- src/STRING003.java
+++ src/STRING003.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-public class STRING002 {
+public class STRING003 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         sc.nextLine();
         while (t-- > 0) {
             String s = sc.next();
-            int i = sc.nextInt();
-            System.out.println("The character at position " + i + " is " + s.charAt(i));
+            StringBuffer sb = new StringBuffer(s);
+            System.out.println(sb.length());
         }
         sc.close();
     }
 }
+//Sử dụng Stringbuffer để lấy chiều dài chuỗi
