87ab8940ded0f4d2d65b5fdd6f56af9906492063
SIC1740 <SiVV.B21VT378@stu.ptit.edu.vn>
2025-03-21 18:19:07 +0700

    repard

--- src/COLLECTION001.java
+++ src/COLLECTION001.java
@@ -1,8 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Product {
+    private int price;
+    private int stock;
+
+    public Product(int price, int stock) {
+        this.price = price;
+        this.stock = stock;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public int getStock(){
+        return stock;
+    }
+}
+class Warehouse{
+    private Map<String, Product> products;
+    public Warehouse(){
+        products = new HashMap<>();
+    }
+    public void addProduct(String product, int price, int stock){
+        products.put(product,new Product(price,stock));
+    }
+    public String price(String product){
+        if(products.containsKey(product)) {
+            return product + ": " + products.get(product).getPrice();
+        }
+        else {
+            return product + ": -99";
+        }
+    }
+}
 public class COLLECTION001 {
     public static void main(String[] args) {
-        System.out.println("prices:\n" +
-                "milk: 3\n" +
-                "coffee: 5\n" +
-                "sugar: -99");
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProduct("milk",3,10);
+        warehouse.addProduct("coffee",5,7);
+
+        System.out.println("prices:");
+        System.out.println(warehouse.price("milk"));
+        System.out.println(warehouse.price("coffee"));
+        System.out.println(warehouse.price("sugar"));
     }
 }
